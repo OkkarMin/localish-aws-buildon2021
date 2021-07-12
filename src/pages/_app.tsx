49eda5cb24634f45d0dfa28315d9749208939580
Okkar Min <OMIN001@e.ntu.edu.sg>
2021-07-12 10:52:59 +0800

    feat: :mag: Add SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
 
 import theme from "../theme";
 
@@ -8,6 +9,51 @@ import NavBar from "../components/layout/navbar";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
+      <DefaultSeo
+        title="localish | hyper-local-communities"
+        description="Built for communities within clusters to bond over shared interests 🤝"
+        canonical="https://localish.ml"
+        openGraph={{
+          title: "localish | hyper-local-communities",
+          description:
+            "Built for communities within clusters to bond over shared interests 🤝",
+          url: "https://localish.ml",
+          images: [
+            {
+              url: "/meta_image.png",
+              width: 1200,
+              height: 628,
+              alt: "Localish site's meta image",
+            },
+          ],
+        }}
+        twitter={{
+          cardType: "summary_large_image",
+        }}
+        additionalLinkTags={[
+          {
+            rel: "apple-touch-icon",
+            sizes: "180x180",
+            href: "/apple-touch-icon.png",
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            sizes: "32x32",
+            href: "/favicon-32x32.png",
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            sizes: "16x16",
+            href: "/favicon-16x16.png",
+          },
+          {
+            rel: "manifest",
+            href: "/site.webmanifest",
+          },
+        ]}
+      />
       <NavBar />
       <Component {...pageProps} />
     </ChakraProvider>
