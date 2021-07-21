31ad9a93ffebc7b37b192461b933dfb8666f1e06
Okkar Min <OMIN001@e.ntu.edu.sg>
2021-07-21 16:42:49 +0800

    chore: :recycle: Refactor education

--- src/pages/education.tsx
+++ src/pages/education.tsx
@@ -1,18 +1,28 @@
 import React, { useEffect, useState } from "react";
 import Parser from "rss-parser";
-
-import {
-  Box,
-  Button,
-  chakra,
-  Flex,
-  Image,
-  Link,
-  SimpleGrid,
-  useColorModeValue,
-} from "@chakra-ui/react";
-
 const RSSParser = new Parser();
+
+import { Box, Button, chakra, Link, SimpleGrid } from "@chakra-ui/react";
+
+const EducationCard = ({ title, content, link }) => {
+  return (
+    <Box mx={{ lg: 8 }} shadow={{ lg: "lg" }} rounded={{ lg: "lg" }}>
+      <Box p={6} maxW={{ base: "xl", lg: "5xl" }}>
+        <chakra.h2 fontSize={{ base: "2xl", md: "3xl" }} fontWeight="bold">
+          {title}
+        </chakra.h2>
+
+        <chakra.p mt={4}>{content}</chakra.p>
+
+        <Link isExternal href={link} _hover={{ textDecoration: "none" }}>
+          <Button mt={8} colorScheme="greenPrimary">
+            Read more
+          </Button>
+        </Link>
+      </Box>
+    </Box>
+  );
+};
 
 const Education = () => {
   const [feed, setFeed] = useState([]);
@@ -46,23 +56,3 @@ const Education = () => {
 };
 
 export default Education;
-
-const EducationCard = ({ title, content, link }) => {
-  return (
-    <Box mx={{ lg: 8 }} shadow={{ lg: "lg" }} rounded={{ lg: "lg" }}>
-      <Box p={6} maxW={{ base: "xl", lg: "5xl" }}>
-        <chakra.h2 fontSize={{ base: "2xl", md: "3xl" }} fontWeight="bold">
-          {title}
-        </chakra.h2>
-
-        <chakra.p mt={4}>{content}</chakra.p>
-
-        <Link isExternal href={link} _hover={{ textDecoration: "none" }}>
-          <Button mt={8} colorScheme="greenPrimary">
-            Read more
-          </Button>
-        </Link>
-      </Box>
-    </Box>
-  );
-};
